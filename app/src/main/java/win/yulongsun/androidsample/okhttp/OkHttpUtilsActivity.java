f7c976594e70847476ac45d984c46a437b2dda4d
yulongsun <yuls@jiudeng.me>
2016-05-04 11:47:03 +0800

    add okhttputils

---
+++ app/src/main/java/win/yulongsun/androidsample/okhttp/OkHttpUtilsActivity.java
@@ -0,0 +1,17 @@
+package win.yulongsun.androidsample.okhttp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import win.yulongsun.androidsample.R;
+
+public class OkHttpUtilsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ok_http_utils);
+    }
+}
